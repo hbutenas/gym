c6578d0254d5c651fc2f7ddc8d6a58a9d16adc1c
Henrikas Butenas <butenashenrikas@gmail.com>
2022-09-21 16:39:34 +0300

    Update auth.service.ts

--- server/src/auth/services/auth/auth.service.ts
+++ server/src/auth/services/auth/auth.service.ts
@@ -1,5 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDto } from 'src/auth/dto/register.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  public async register(body: RegisterDto) {
+    // destruct body
+    const { email, username, password, firstName, lastName } = body;
+
+    // check for existing user by email
+    const userExists = await this.prisma['User'].findFirst({
+      where: {
+        email: email.toLowerCase(),
+      },
+    });
+    
     // couldn't find existing user
+  }
+
+  public async login() {
+    // TODO Login
+  }
+}
+//
