ca56bc09fe6ad00ef9f912cea99d9d082ed5e224
Henrikas Butenas <butenashenrikas@gmail.com>
2022-09-21 16:39:46 +0300

    Modules setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/controllers/auth/auth.controller';
-import { AuthService } from './auth/services/auth/auth.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
 })
 export class AppModule {}
