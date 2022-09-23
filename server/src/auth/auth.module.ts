f225f76c96569e863b89ffda46e75bbcdaa33b51
Henrikas Butenas <butenashenrikas@gmail.com>
2022-09-23 16:13:58 +0300

    JwtModule included to auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
